eca7a18914247f51cd81b75659f8d7a86ea0834a
mrimadali <mrimadali@gmail.com>
2014-08-07 18:01:39 +0530

    second commit

--- GitDemo/AppDelegate.m
+++ GitDemo/AppDelegate.m
@@ -19,6 +19,9 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    
+    //Added new comment..
     [self.window makeKeyAndVisible];
     return YES;
 }
